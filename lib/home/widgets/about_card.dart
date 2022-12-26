82e3b9b1f095f79520b62953a4b4321a24355af1
jesusrp98 <jesusrope98@outlook.com>
2022-12-26 10:37:23 +0100

    Update about card UI

--- lib/home/widgets/about_card.dart
+++ lib/home/widgets/about_card.dart
@@ -1,11 +1,10 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:layout/layout.dart';
 import 'package:portfolio/home/models/experience.dart';
 import 'package:portfolio/home/widgets/home_card.dart';
 
 class AboutCard extends StatelessWidget {
-  static const _contentHeight = 176.0;
+  static const _imageDimension = 164.0;
 
   final Experience experience;
 
@@ -16,47 +15,48 @@ class AboutCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HomeCard(
-      child: SizedBox(
-        height: _contentHeight,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.asset(
+    return ConstrainedBox(
+      constraints: const BoxConstraints(minHeight: _imageDimension),
+      child: HomeCard(
+        padding: EdgeInsets.zero,
+        child: IntrinsicHeight(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset(
                 experience.imagePath,
-                height: _contentHeight,
-                width: _contentHeight,
+                width: _imageDimension,
                 fit: BoxFit.cover,
               ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    experience.title,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 26,
-                    ),
-                  ),
-                  ...[
-                    for (final detail in experience.details)
-                      AutoSizeText(
-                        detail,
-                        maxLines: 3,
-                        style: TextStyle(
-                          color: Theme.of(context).textTheme.caption?.color,
-                          fontSize: 16,
+              Expanded(
+                child: Padding(
+                  padding: HomeCard.resolvePadding(context),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        experience.title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 26,
                         ),
                       ),
-                  ],
-                ].separate(16),
-              ),
-            )
-          ].separate(24),
+                      ...[
+                        for (final detail in experience.details)
+                          Text(
+                            detail,
+                            style: TextStyle(
+                              color: Theme.of(context).textTheme.caption?.color,
+                              fontSize: 18,
+                            ),
+                          ),
+                      ],
+                    ].separate(16),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/home/widgets/home_card.dart
+++ lib/home/widgets/home_card.dart
@@ -2,34 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:layout/layout.dart';
 
 class HomeCard extends StatelessWidget {
+  final EdgeInsets? padding;
   final Widget child;
 
   const HomeCard({
     super.key,
+    this.padding,
     required this.child,
   });
 
+  static EdgeInsets resolvePadding(BuildContext context) =>
+      context.layout.value(
+        xs: const EdgeInsets.all(16),
+        md: const EdgeInsets.all(24),
+      );
+
   @override
   Widget build(BuildContext context) {
-    final padding = context.layout.value(
-      xs: const EdgeInsets.all(16),
-      md: const EdgeInsets.all(24),
-    );
-
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        color: Theme.of(context).cardColor,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 12,
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: padding,
-        child: child,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 12,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: padding ?? resolvePadding(context),
+          child: child,
+        ),
       ),
     );
   }
