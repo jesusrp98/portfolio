554cf16fd1595046b073af9c72541c1e611d7ee4
jesusrp98 <jesusrope98@outlook.com>
2024-02-18 19:10:26 +0100

    Update deps

--- lib/utils/portfolio_urls.dart
+++ lib/utils/portfolio_urls.dart
@@ -3,7 +3,7 @@ class PortfolioUrls {
 
   static const gitHub = 'https://github.com/jesusrp98/';
   static const linkedIn = 'https://www.linkedin.com/in/jesusrp98/';
-  static const email = 'mailto:jesusrope98@outlook.com';
+  static const email = 'mailto:work@jesusrp98.com';
   static const portfolioGitHubPage = 'https://github.com/jesusrp98/portfolio/';
   static const flutterPage = 'https://flutter.dev/';
 }
